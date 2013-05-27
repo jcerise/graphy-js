5a50ea358cf5356127dbcff172bca5d8f33963f2
jcerise <jcerise06@gmail.com>
2013-05-27 11:35:46 -0600

    implementing graph pages

--- client/main.js
+++ client/main.js
@@ -4,4 +4,32 @@ Template.graphy.logged_in = function() {
 
 Template.graphy.graph_selected = function() {
     return Session.get("current_graph");
-}
+};
+
+Template.graphs_page.has_graphs = function() {
+    return GraphsModel.find({}).count();
+};
+
+Template.graphs_page.graphs = function() {
+    return GraphsModel.find({});
+};
+
+Template.graphs_page.events({
+    'click #new_graph' : function() {
+        Graphy.Graphs.create("new Graph (Double Click to rename)", Meteor.userId());
+    },
+    'dblclick .graph_title' : function() {
+        var new_name = prompt("Please enter a new name:", this.name);
+        if (new_name) {
+            Graphy.Graphs.rename(this._id, new_name);
+        }
+    },
+    'click .delete_graph' : function() {
+        if (confirm("Are you sure you want to delete " + this.name)) {
+            Grapher.Graphs.delete(this._id);
+        }
+    },
+    'click .open_graph' : function() {
+        Grapher.Graphs.change_current(this);
+    }
+});
